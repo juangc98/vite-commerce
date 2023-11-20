b6cc26acc502fd36a5be66fe3329631774a00903
Juangc98 <juangc_98@hotmail.com>
2023-11-20 13:04:17 -0300

    implementing firebase without success

--- src/components/Routing.jsx
+++ src/components/Routing.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
+import { collection, getDoc, getFirestore } from "firebase/firestore";
+
 import Home from '../pages/HomePage';
 import ProductPage from '../pages/ProductPage';
 import Cart from '../pages/CartPage';
@@ -16,12 +18,24 @@ import '../App.css'
 import {storeData} from '../assets/data.js';
 const apiUrl = import.meta.env.VITE_API_URL;
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBk6El6ddX27sGKSFu-olwjeFaJrJkpkAM",
+  authDomain: "calcio-ecommerce.firebaseapp.com",
+  projectId: "calcio-ecommerce",
+  storageBucket: "calcio-ecommerce.appspot.com",
+  messagingSenderId: "357598994956",
+  appId: "1:357598994956:web:5aa779a0aa3329a34f4429"
+};
+const db = getFirestore(firebase.initializeApp(firebaseConfig));
 
 const Routing = () => {
+    const [data, setData] = useState([]);
     const [categories, setCategories] = useState([]);
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const colRef = collection(db, 'productos');
     
     const toggleMenuDrawer = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -66,13 +80,26 @@ const Routing = () => {
           setLoading(false);
       })*/
     }
+    const fetchFData = async () => {
+      try {
+        const querySnapshot = await getDocs(colRef);
+        const documents = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setData(documents);
+      } catch (error) {
+        console.error('Error al obtener datos de Firestore:', error);
+      }
+    };
     useEffect(() => {
-      getData()
-        .then(res => {
+      fetchFData()
+      // getData()
+        /*.then(res => {
           setProducts(res.products);
           setCategories(res.categories);
           setLoading(false);
-        })
+        })*/
       }, []);
 
     if ( loading ) {
@@ -87,6 +114,9 @@ const Routing = () => {
         <>
             <BrowserRouter>
                 <Navbar categories={categories} isMenuOpen={isMenuOpen} toggleMenu={toggleMenuDrawer} />
+                {data.map((item) => (
+                  <div key={item.id}>{/* Renderiza tus datos aquí */}</div>
+                ))}
                 <Routes>
                     <Route path='/' element={<Home categories={categories} products={products} />} />
                     <Route path='/categoria/:slug' element={<CategoryPage categories={categories} products={products} />} />
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
 
+// Configura tu contexto de Firebase
+const FirebaseContext = createContext();
+
+// Componente que proporciona la instancia de Firebase al contexto
+const FirebaseProvider = ({ children }) => {
+  const [firebaseApp, setFirebaseApp] = useState(null);
+
+  useEffect(() => {
+    const app = initializeApp(firebaseConfig);
+    setFirebaseApp(app);
+  }, []);
+
+  return (
+    <FirebaseContext.Provider value={firebaseApp}>
+      {children}
+    </FirebaseContext.Provider>
+  );
+};
+
+// Función para usar la instancia de Firebase en otros componentes
+const useFirebase = () => {
+  const context = useContext(FirebaseContext);
+  if (!context) {
+    throw new Error('useFirebase debe ser utilizado dentro de FirebaseProvider');
+  }
+  return context;
+};
+
+// Envuelve tu aplicación con FirebaseProvider para proporcionar la instancia de Firebase
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <FirebaseProvider>
+      <App />
+    </FirebaseProvider>
   </React.StrictMode>,
-)
+);
